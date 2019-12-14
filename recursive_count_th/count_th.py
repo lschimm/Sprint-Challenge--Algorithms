0ec1f1e7e4d3b5813bac2058013c0d7c6b0b0b64
lschimm <lschimmelfennig@gmail.com>
2019-12-13 20:20:30 -0600

    Adds planning

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,14 @@
 '''
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurences
+of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    # Plan 1:
+    # Base case should be if a word is < 2, if so, return 0
+    # then...basically go through each of the letters looking for a paired 'th'
+
     
-    # TBC
     
     pass
