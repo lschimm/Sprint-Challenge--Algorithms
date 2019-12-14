3bd055583a529124fb70f888c4c20663e0be220b
lschimm <lschimmelfennig@gmail.com>
2019-12-13 20:44:32 -0600

    Finishes count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,8 +7,21 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     # Plan 1:
     # Base case should be if a word is < 2, if so, return 0
-    # then...basically go through each of the letters looking for a paired 'th'
+    # Will not count capitalized TH
+    # then...basically go through each of the letters (two at a time)
+    # looking for a paired 'th'
+    # if one is found, add to the count
+    # if there isn't any, and nothing to the count
 
-    
+    if len(word) < 2:
+        return 0
+    elif word[:2] == "th":
+        return 1 + count_th(word[1:])
+        print("********", word[1:])
+    else:
+        return 0 + count_th(word[1:])
     
     pass
+
+print(count_th(""))
+print(count_th("thTHthTHthTH"))
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,7 +96,7 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
+
         pass
 
 
